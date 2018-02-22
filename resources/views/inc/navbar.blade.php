98350ae6fc1bcee55f5cc05ee175e1dfa398bbdd
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-21 21:31:01 -0800

    Added authentication, and posts are now tied to their owner

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,29 +1,60 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-3">
-    <a class="navbar-brand" href="{{config('app.url')}}">{{config('app.name', 'Laravel Intro')}}</a>
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            {{ config('app.name', 'Laravel') }}
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-    </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{config('app.url')}}">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{config('app.url')}}/about">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{config('app.url')}}/services">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{config('app.url')}}/posts">Blog</a>
+                </li>
+            </ul>
 
-    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="{{config('app.url')}}">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{config('app.url')}}/about">About</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{config('app.url')}}/services">Services</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{config('app.url')}}/posts">Blog</a>
-            </li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-            <li class="nav-item">
-                <a class="nav-link" href="{{config('app.url')}}/posts/create">Create Post</a>
-            </li>
-        </ul>
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                @guest
+                    <li><a class="nav-link" href="{{ route('login') }}">Login</a></li>
+                    <li><a class="nav-link" href="{{ route('register') }}">Register</a></li>
+                @else
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{config('app.url')}}/posts/create">Create Post</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{config('app.url')}}/dashboard">
+                                Dashboard
+                            </a>
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                                Logout
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
+        </div>
     </div>
 </nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,29 +1,34 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{config('app.name', 'Laravel Intro')}}</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" 
-        rel="stylesheet" type="text/css">
-        <link href="{{asset('css/app.css')}}" rel="stylesheet">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        
-    </head>
-    <body>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
         @include('inc.navbar')
         <div class="container">
             @include('inc.messages')
             @yield('content')
         </div>
+    </div>
 
-        <script src="{{asset('/vendor/unisharp/laravel-ckeditor/ckeditor.js')}}"></script>
-        <script>
-            CKEDITOR.replace( 'article-ckeditor' );
-        </script>
-    </body>
+    <!-- Scripts -->
+    
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{asset('/vendor/unisharp/laravel-ckeditor/ckeditor.js')}}"></script>
+    <script>
+        CKEDITOR.replace( 'article-ckeditor' );
+    </script>
+    
+</body>
 </html>
