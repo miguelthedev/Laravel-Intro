d67d43460e381cb0d4d48a4ef6a72fe8f5ebab86
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-21 22:20:19 -0800

    Added a dashboard for individual users

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,8 +13,32 @@
                             {{ session('status') }}
                         </div>
                     @endif
+
                     <a href="{{ url('/posts/create') }}" class="btn btn-primary mb-3">Create Post</a>
                     <h3>Your Blog Posts</h3>
+
+                    @if(count($posts) > 0)
+                        <table class="table table-striped">
+                            <tr>
+                                <th>Title</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
+                            </tr>
+                        
+                            @foreach($posts as $post)
+                                <tr>
+                                    <th>{{$post->title}}</th>
+                                    <th><a href="{{ url('/posts/'.$post->id.'/edit') }}" class="btn btn-warning">Edit</a></th>
+                                    <th>
+                                        {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => ''])!!}
+                                            {{Form::hidden('_method', 'DELETE')}}
+                                            {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+                                        {!!Form::close()!!}
+                                    </th> 
+                                </tr>
+                            @endforeach
+                        </table>
+                    @endif
                 </div>
             </div>
         </div>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,7 +6,7 @@
         @foreach($posts as $post)
             <div class="card card-body mb-3">
                 <h3><a href="{{config('app.url')}}/posts/{{$post->id}}">{{$post->title}}</a></h3>
-                <small>Written on {{$post->created_at}}</small>
+                <small>Written on {{$post->created_at}} {{$post->user->name}}</small>
             </div>
         @endforeach
         {{$posts->links()}}
